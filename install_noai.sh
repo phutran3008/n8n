40fc312026d5d1f5c84efb7ef9a988e27c9efc18
phutran3008 <phutran3008@gmail.com>
2025-04-28 21:35:00 +0700

    Update install_noai.sh

--- install_noai.sh
+++ install_noai.sh
@@ -14,8 +14,8 @@ sudo chown -R 1000:1000 vol_n8n
 sudo chmod -R 755 vol_n8n
 echo "--------- 🔴 Finish creating folder -----------"
 echo "--------- 🟢 Start docker compose up  -----------"
-wget https://raw.githubusercontent.com/phutran3008/n8n/refs/heads/main/compose_noai.yaml -O compose.yaml
-export EXTERNAL_IP=http://"$(hostname -I | cut -f1 -d' ')"
 export CURR_DIR=$(pwd)
+export EXTERNAL_IP=http://"$(hostname -I | cut -f1 -d' ')"
+wget https://raw.githubusercontent.com/phutran3008/n8n/refs/heads/main/compose_noai.yaml -O compose.yaml
 sudo -E docker compose up -d
 echo "--------- 🔴 Finish! Wait a few minutes and test in browser at url $EXTERNAL_IP for n8n UI -----------"
